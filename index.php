5bae2bce3235d1599e01bdaf52ffaa4171844d77
Thomas Aldaitz <thomas.aldaitz@wildcodeschool.com>
2022-10-12 10:10:05 +0200

    First step

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ma collection de vinyl !</title>
+</head>
+<body>
+
+    <header>
+    <!--
+        - Mobile : 
+            - Doit contenir un titre "Ma collection de vinyl" en Arial de 28px calé sur la gauche
+            - tout le header doit avoir un background Noir et la police doit etre orange
+
+        - Desktop:
+            - Le titre doit se centrer sur la page
+            - juste en dessous doit apparaitre une citation sur la musique
+            - le background doit devenir une photo de vinyl et la police doit repasser en blanc
+            et en Silkscreen (https://fonts.google.com/specimen/Silkscreen)
+    -->
+    </header>
+
+    <nav>
+    <!--
+        - Mobile : 
+           - la liste (ul) des liens s'affiche en colonne :
+                - Accueil (index.php)
+                - Ajouter (create.php)
+        
+        - Desktop : 
+            - Afficher la liste des liens calé à droite de la page
+    -->
+    </nav>
+
+    <main>
+    <!--
+        - Mobile : 
+           - Présenter dans un cadre un vinyl : avec son titre, sa pochette, 
+                et le nom de l'artiste
+           - Présenter au moins 6 vinyl sur la page
+        
+        - Desktop : 
+          - Afficher les même cadre, mais cette trois sur la même ligne
+    -->
+    </main>
+
+    <footer>
+    <!--
+        - Mobile : 
+          - Ne doit pas apparaitre
+
+        - Desktop :
+          - doit afficher centrer sur la page, la liste des membres du groupe avec leur noms, email et compte github
+    -->
+    </footer>
+    
+</body>
+</html>
