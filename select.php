a80e358c738ff9f213675bff2116f7837f5fc0c6
Thomas Aldaitz <thomas.aldaitz@wildcodeschool.com>
2022-10-12 10:59:08 +0200

    First Step

---
+++ select.php
@@ -0,0 +1,14 @@
+<?php
+ /*
+    1 - Creer une table Vinyl en BDD : 
+       - id Int PK AI
+       - title String 200 char max
+       - cover String 255 char max
+       - artist String 200 char max
+       - genre String 100 char max
+
+    2 - Saisir au moins 5 lignes d'exemples
+
+    3 - Ecrire le code PHP qui permet de récupérer ces lignes en BDD pour les afficher 
+    dans un joli tableau HTML :-D 
+ */
