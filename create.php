6ca011c7b44b5992a9ecd8fa1253d8de3a34586a
Thomas Aldaitz <thomas.aldaitz@wildcodeschool.com>
2022-10-12 10:25:19 +0200

    First step

---
+++ create.php
@@ -0,0 +1,29 @@
+<?php
+/*
+    1/ Créer un formulaire pour renseigner un nouveau vinyl : 
+        - Titre => string de Max 200 caractères - Obligatoire
+        - Pochette => Url - Obligatoire
+        - Artiste => string de max 200 caractères - Obligatoire
+        - Genre => liste déroulante contenant les valeurs : Rock, Classique, Rap, Jazz, Electro, BO de film, Métal, Disco
+
+        Le formulaire doit envoyer ces informations sur la même page
+
+    2/ Controle des données saisies : 
+       - vous devez afficher "Bravo le nouveau Vinyl a bien été transmis !" si les données correspondent bien au critères ci-dessus
+       - vous devez affichez "Non essayez encore";
+       - Et si le Genre selectionné est "Disco" vous devez affichez "Merci de bien vouloir brûler ce vinyl et s'il vous plait n'utiliser plus cette plateforme."
+*/
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajouter un nouveau vinyl</title>
+</head>
+<body>
+    
+</body>
+</html>
